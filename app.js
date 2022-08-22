42303428a1aa2b636742a572a950768da6c5f7a9
alikedo <alikemaldogan88@gmail.com>
2022-08-22 17:31:31 +0200

    app.js updated

--- app.js
+++ app.js
@@ -1,20 +1,3 @@
-// ℹ️ Gets access to environment variables/settings
-// https://www.npmjs.com/package/dotenv
-require("dotenv/config");
-
-// ℹ️ Connects to the database
-require("./db");
-
-// Handles http requests (express is node js framework)
-// https://www.npmjs.com/package/express
-const express = require("express");
-
-// Handles the handlebars
-// https://www.npmjs.com/package/hbs
-const hbs = require("hbs");
-
-const app = express();
-
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
@@ -27,6 +10,9 @@ app.locals.appTitle = `${capitalized(projectName)} created with IronLauncher`;
 // 👇 Start handling routes here
 const index = require("./routes/index.routes");
 app.use("/", index);
+
+const auth = require("./routes/auths.routes");
+app.use("/auth", auth);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
